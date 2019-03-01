d16f054b8f4c15c2a404e27e35c58f2dab2d414b
Luka <luka.teng@ximalaya.com>
2019-03-01 18:28:54 +0800

    优化webpackPlugin的store路径

--- config/webpackPlugins/Ignore-store-plugin.js
+++ config/webpackPlugins/Ignore-store-plugin.js
@@ -8,13 +8,16 @@ class IgonreStore {
     compiler.hooks.normalModuleFactory.tap('nmf', (nmf) => {
       nmf.hooks.beforeResolve.tap("bs", (result) => {  
         let { context, request } = result
+        const tempStore = path.resolve(process.cwd(), 'src/store.temp.js')
+
         for (let key in alias) {
           const reg = new RegExp(key, 'g')
           request = request.replace(reg, alias[key])
         }
+
         let filePath = path.resolve(context, request)
         if (/\/store/.test(request) && !fs.existsSync(filePath)) {
-          result.request = '~/store.temp.js'
+          result.request = tempStore
         }
         return result
       })
